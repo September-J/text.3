32b89ede00d4aeaebb58527842a6c8d62aad15c3
September J <2286954217@qq.com>
2019-04-13 16:35:14 +0800

    text.3

---
+++ text.3.cpp
@@ -0,0 +1,53 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//void printArray(int a[], int n){
+//	int i;
+//	for (i = 0; i < n; ++i){
+//		printf("%d", a[i]);
+//	}
+//	putchar('\n');
+//}
+//
+//int main(){
+//	int a[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//	int b[10] = { 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+//	int i, tmp;
+//	printArray(a, 10);
+//	printArray(b, 10);
+//
+//	for (i = 0; i < 10; ++i){
+//		tmp = a[i];
+//		a[i] = b[i];
+//		b[i] = tmp;
+//	}
+//	printArray(a, 10);
+//	printArray(b, 10);
+//	system("pause");
+//	return 0;
+//}
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main_(){
+//	/*int i;
+//	double tmp = 1, sum = 0, flag = 1;
+//	for (i = 1; i <= 100; ++i){
+//		tmp = 1.0 / i*flag;
+//		sum += tmp;
+//		flag *= -1;
+//	}
+//	printf("%lf\n", sum);*/
+//	/*int i, count = 0;
+//	for (i = 1; i <= 100; ++i){
+//		if (i % 10 == 9){
+//			count++;
+//		}
+//		if (i / 10 % 10 == 9){
+//			count++;
+//		}
+//	}
+//	printf("%d\n", count);
+//	system("pause");
+//	return 0;
+//}*/
